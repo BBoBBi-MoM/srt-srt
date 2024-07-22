09e01a8f7a9508ee595d8bed71af653241f3fd00
lhool <shanghai110930@gmail.com>
2024-07-23 00:10:55 +0900

    add: FastAPI web server module

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ async def submit_form(request: Request, name: str = Form(...), departure: str = 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)

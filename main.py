a180b7588a9ad4354faa100c59852f7478ad70b9
lhool <shanghai110930@gmail.com>
2024-07-25 00:40:18 +0900

    update: autopep8 pre-commit test..

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -16,18 +17,18 @@ async def read_root(request: Request):
 
 @app.post("/submit", response_class=HTMLResponse)
 async def submit_form(
-    request: Request, 
-    id: str = Form(...), 
-    password: str = Form(...), 
+    request: Request,
+    id: str = Form(...),
+    password: str = Form(...),
     departure: str = Form(...),
-    destination: str = Form(...), 
-    min_time: str = Form(...), 
-    max_time: str = Form(...), 
+    destination: str = Form(...),
+    min_time: str = Form(...),
+    max_time: str = Form(...),
     best_time: str = Form(None),
     route: str = Form(...),
     adult_count: str = Form(...),
     child_count: str = Form(...),
-    senior_count: str = Form(...),
+    elder_count: str = Form(...),
     severe_disabled_count: str = Form(...),
     mild_disabled_count: str = Form(...),
     seat: str = Form(...),
@@ -42,13 +43,13 @@ async def submit_form(
         "route": route,
         "adult_count": adult_count,
         "child_count": child_count,
-        "senior_count": senior_count,
+        "elder_count": elder_count,
         "severe_disabled_count": severe_disabled_count,
         "mild_disabled_count": mild_disabled_count,
         "seat": seat,
         "train_type": train_type
     }
-    
+
     return templates.TemplateResponse("result.html", {"request": request, **context})
 
 if __name__ == "__main__":
